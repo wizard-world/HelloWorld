4bf3b7598825ee7049e4291eb5a3a937ec87400c
wizard.liyd <wizard.liyd@outlook.com>
2013-01-30 14:33:20 +0800

    新建

---
+++ src/com/java/regex.java
@@ -0,0 +1,21 @@
+package com.java;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class regex {
+    
+    private static String str = "abcccccccccc";
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Pattern p = Pattern.compile("^\\w+([A-Za-z0-9/:;_.-]\\S)*");
+        Matcher m = p.matcher((CharSequence) str);
+        boolean b = m.matches();
+        System.out.println(b);
+    }
+
+}
